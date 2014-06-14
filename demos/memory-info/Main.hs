e22b41b59e0e7e6916e16dbf834ef2ebddcd8645
Mikko Juola <mikjuo@gmail.com>
2014-06-14 20:07:18 +0900

    Add a memory info demo.

---
+++ demos/memory-info/Main.hs
@@ -0,0 +1,23 @@
+module Main ( main ) where
+
+import Caramia
+import Graphics.UI.SDL
+import Data.Bits
+import Foreign.C.String
+
+main :: IO ()
+main = 
+    withCString "smoke-test" $ \cstr -> do
+        _ <- glSetAttribute glAttrContextMajorVersion 3
+        _ <- glSetAttribute glAttrContextMinorVersion 2
+        _ <- glSetAttribute glAttrContextProfileMask glProfileCore
+        _ <- glSetAttribute glAttrContextFlags glContextFlagDebug
+        window <- createWindow cstr windowPosUndefined windowPosUndefined
+                                    500 500
+                                    (windowFlagOpenGL .|.
+                                     windowFlagShown)
+        _ <- glCreateContext window
+        giveContext $ do
+            putStrLn "Attempting to retrieve GPU memory information..."
+            print =<< getMemoryInfo
+
