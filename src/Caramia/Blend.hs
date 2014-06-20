098ae804d8c58d71227293be68b06413d82fdd6d
Mikko Juola <mikjuo@gmail.com>
2014-06-20 21:13:14 +0900

    Add a `nopBlend` blending mode.

--- src/Caramia/Blend.hs
+++ src/Caramia/Blend.hs
@@ -10,7 +10,8 @@ module Caramia.Blend
     , BlendEquation(..)
     , BlendFunc(..)
     -- * Pre-defined blending specs
-    , preMultipliedAlpha )
+    , preMultipliedAlpha
+    , nopBlend )
     where
 
 --import Caramia.Prelude
@@ -23,6 +24,18 @@ preMultipliedAlpha = BlendSpec
     , srcAlphaFunc = BFOne
     , dstColorFunc = BFOneMinusSrcAlpha
     , dstAlphaFunc = BFOneMinusSrcAlpha
+    , colorEquation = BEAdd
+    , alphaEquation = BEAdd
+    , blendColor = rgba 1 1 1 1 }
+
+-- | Blending that doesn't do anything special; just copies the source pixel to
+-- destination.
+nopBlend :: BlendSpec
+nopBlend = BlendSpec
+    { srcColorFunc = BFOne
+    , srcAlphaFunc = BFOne
+    , dstColorFunc = BFZero
+    , dstAlphaFunc = BFZero
     , colorEquation = BEAdd
     , alphaEquation = BEAdd
     , blendColor = rgba 1 1 1 1 }
