e0b89b554a3ba69c14be809d5af9c4c8fd880414
Nikolay Amiantov <ab@fmap.me>
2014-12-03 21:37:55 +0300

    Remove redundant type singatures

--- src/Graphics/Caramia/Math.hs
+++ src/Graphics/Caramia/Math.hs
@@ -249,31 +249,31 @@ instance Storable Vector3 where
     sizeOf _ = sizeOf (undefined :: Float) * 3
     alignment _ = alignment (undefined :: Float)
     peek ptr = {-# SCC "Vector3_peek" #-} do
-        x <- peekElemOff cptr 0 :: IO Float
-        y <- peekElemOff cptr 1 :: IO Float
-        z <- peekElemOff cptr 2 :: IO Float
+        x <- peekElemOff cptr 0
+        y <- peekElemOff cptr 1
+        z <- peekElemOff cptr 2
         return $ Vector3 x y z
       where
-        cptr = castPtr ptr :: Ptr Float
+        cptr = castPtr ptr
 
     poke ptr (Vector3 x y z) = {-# SCC "Vector3_poke" #-} do
         pokeElemOff cptr 0 x
         pokeElemOff cptr 1 y
         pokeElemOff cptr 2 z
       where
-        cptr = castPtr ptr :: Ptr Float
+        cptr = castPtr ptr
 
 instance Storable Quaternion where
     sizeOf _ = sizeOf (undefined :: Float) * 4
     alignment _ = alignment (undefined :: Float)
     peek ptr = {-# SCC "Quaternion_peek" #-} do
-        x <- peekElemOff cptr 0 :: IO Float
-        y <- peekElemOff cptr 1 :: IO Float
-        z <- peekElemOff cptr 2 :: IO Float
-        w <- peekElemOff cptr 3 :: IO Float
+        x <- peekElemOff cptr 0
+        y <- peekElemOff cptr 1
+        z <- peekElemOff cptr 2
+        w <- peekElemOff cptr 3
         return $ Quaternion x y z w
       where
-        cptr = castPtr ptr :: Ptr Float
+        cptr = castPtr ptr
 
     poke ptr (Quaternion x y z w) = {-# SCC "Quaternion_poke" #-} do
         pokeElemOff cptr 0 x
@@ -281,34 +281,34 @@ instance Storable Quaternion where
         pokeElemOff cptr 2 z
         pokeElemOff cptr 3 w
       where
-        cptr = castPtr ptr :: Ptr Float
+        cptr = castPtr ptr
 
 instance Storable Matrix44 where
     sizeOf _ = sizeOf (undefined :: Float) * 16
     alignment _ = alignment (undefined :: Float)
     peek ptr = {-# SCC "Matrix44_peek" #-} do
-        r11 <- peekElemOff cptr 0 :: IO Float
-        r21 <- peekElemOff cptr 1 :: IO Float
-        r31 <- peekElemOff cptr 2 :: IO Float
-        r41 <- peekElemOff cptr 3 :: IO Float
-        r12 <- peekElemOff cptr 4 :: IO Float
-        r22 <- peekElemOff cptr 5 :: IO Float
-        r32 <- peekElemOff cptr 6 :: IO Float
-        r42 <- peekElemOff cptr 7 :: IO Float
-        r13 <- peekElemOff cptr 8 :: IO Float
-        r23 <- peekElemOff cptr 9 :: IO Float
-        r33 <- peekElemOff cptr 10 :: IO Float
-        r43 <- peekElemOff cptr 11 :: IO Float
-        r14 <- peekElemOff cptr 12 :: IO Float
-        r24 <- peekElemOff cptr 13 :: IO Float
-        r34 <- peekElemOff cptr 14 :: IO Float
-        r44 <- peekElemOff cptr 15 :: IO Float
+        r11 <- peekElemOff cptr 0
+        r21 <- peekElemOff cptr 1
+        r31 <- peekElemOff cptr 2
+        r41 <- peekElemOff cptr 3
+        r12 <- peekElemOff cptr 4
+        r22 <- peekElemOff cptr 5
+        r32 <- peekElemOff cptr 6
+        r42 <- peekElemOff cptr 7
+        r13 <- peekElemOff cptr 8
+        r23 <- peekElemOff cptr 9
+        r33 <- peekElemOff cptr 10
+        r43 <- peekElemOff cptr 11
+        r14 <- peekElemOff cptr 12
+        r24 <- peekElemOff cptr 13
+        r34 <- peekElemOff cptr 14
+        r44 <- peekElemOff cptr 15
         return $ matrix44 r11 r12 r13 r14
                           r21 r22 r23 r24
                           r31 r32 r33 r34
                           r41 r42 r43 r44
       where
-        cptr = castPtr ptr :: Ptr Float
+        cptr = castPtr ptr
 
     poke ptr mat = {-# SCC "Matrix44_poke" #-} do
         pokeElemOff cptr 0  f11
@@ -328,7 +328,7 @@ instance Storable Matrix44 where
         pokeElemOff cptr 14 f34
         pokeElemOff cptr 15 f44
       where
-        cptr = castPtr ptr :: Ptr Float
+        cptr = castPtr ptr
         f11 = m11 mat
         f21 = m21 mat
         f31 = m31 mat
@@ -350,20 +350,20 @@ instance Storable Matrix33 where
     sizeOf _ = sizeOf (undefined :: CFloat) * 9
     alignment _ = alignment (undefined :: CFloat)
     peek ptr = {-# SCC "Matrix33_peek" #-} do
-        r11 <- peekElemOff cptr 0 :: IO Float
-        r21 <- peekElemOff cptr 1 :: IO Float
-        r31 <- peekElemOff cptr 2 :: IO Float
-        r12 <- peekElemOff cptr 3 :: IO Float
-        r22 <- peekElemOff cptr 4 :: IO Float
-        r32 <- peekElemOff cptr 5 :: IO Float
-        r13 <- peekElemOff cptr 6 :: IO Float
-        r23 <- peekElemOff cptr 7 :: IO Float
-        r33 <- peekElemOff cptr 8 :: IO Float
+        r11 <- peekElemOff cptr 0
+        r21 <- peekElemOff cptr 1
+        r31 <- peekElemOff cptr 2
+        r12 <- peekElemOff cptr 3
+        r22 <- peekElemOff cptr 4
+        r32 <- peekElemOff cptr 5
+        r13 <- peekElemOff cptr 6
+        r23 <- peekElemOff cptr 7
+        r33 <- peekElemOff cptr 8
         return $ matrix33 r11 r12 r13
                           r21 r22 r23
                           r31 r32 r33
       where
-        cptr = castPtr ptr :: Ptr Float
+        cptr = castPtr ptr
 
     poke ptr mat = {-# SCC "Matrix33_poke" #-} do
         pokeElemOff cptr 0 f11
@@ -376,7 +376,7 @@ instance Storable Matrix33 where
         pokeElemOff cptr 7 f23
         pokeElemOff cptr 8 f33
       where
-        cptr = castPtr ptr :: Ptr Float
+        cptr = castPtr ptr
         f11 = n11 mat
         f21 = n21 mat
         f31 = n31 mat
