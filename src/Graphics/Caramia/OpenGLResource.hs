cca0df7a236120cb56c851b125468b95c2df343f
Mikko Juola <mikjuo@gmail.com>
2015-01-29 02:52:03 +0200

    Fix incorrect haddock tags being used in one module.

--- src/Graphics/Caramia/OpenGLResource.hs
+++ src/Graphics/Caramia/OpenGLResource.hs
@@ -14,14 +14,14 @@ import Control.Monad.IO.Class
 -- If you work with raw OpenGL resources, you may need to use `touch` to make
 -- sure the resource is not garbage collected prematurely.
 class OpenGLResource innertype a | a -> innertype where
-    -- ^ Returns the raw OpenGL type.
+    -- | Returns the raw OpenGL type.
     getRaw :: MonadIO m => a -> m innertype
 
-    -- ^ Guarantees that the resource has not been garbage collected at the
+    -- | Guarantees that the resource has not been garbage collected at the
     -- point this function is invoked.
     touch :: MonadIO m => a -> m ()
 
-    -- ^ Promptly finalizes the resource.
+    -- | Promptly finalizes the resource.
     --
     -- This can be unsafe; see `Graphics.Caramia.Resource.newResource`.
     finalize :: MonadIO m => a -> m ()
